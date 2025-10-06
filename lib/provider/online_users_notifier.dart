9ad89908c3c76bea4ee95f8ea022537e52a32bc7
Guilherme Rossoni <guicodin@gmail.com>
2025-10-06 19:09:16 -0300

    update

--- lib/provider/online_users_notifier.dart
+++ lib/provider/online_users_notifier.dart
@@ -5,7 +5,7 @@ class OnlineUsersNotifier extends ChangeNotifier {
   final List<User> _users = [
     User(id: '1', name: 'Anninha', isOnline: true),
     User(id: '2', name: 'Guii'),
-    User(id: '3', name: 'Chiara', isOnline: true),
+    User(id: '3', name: 'Enrique', isOnline: true),
     User(id: '4', name: 'Gigi'),
   ];
 
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -4,7 +4,7 @@ class UserRepository {
   List<User> fetchInitialUsers() => [
     User(id: '1', name: 'Anninha', isOnline: true),
     User(id: '2', name: 'Guii'),
-    User(id: '3', name: 'Chiara', isOnline: true),
+    User(id: '3', name: 'Enrique', isOnline: true),
     User(id: '4', name: 'Gigi'),
   ];
 }
